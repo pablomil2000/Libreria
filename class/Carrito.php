55b931058364442f0f0e721eaef61473707632da
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2021-11-25 20:21:04 +0100

    Update Carrito.php

--- class/Carrito.php
+++ class/Carrito.php
@@ -3,4 +3,42 @@
 class Carrito
 {
 
+<<<<<<< Updated upstream
+=======
+    public function sacarCarro()
+    {
+        if (isset($_SESSION['carrito'])) {
+            $carrito = $_SESSION['carrito'];
+        } else {
+            $carrito = array();
+        }
+        return $carrito;
+    }
+
+    public function addProducto($isbn)
+    {
+        if (isset($carrito) && isset($carrito[$isbn])) {
+            //Existe y sumo
+            $total = $carrito[$isbn];
+            $total++;
+        } else {
+            //no existe y creo
+            $total = 0;
+        }
+        $carrito[$isbn] = $total++;
+        return $carrito;
+    }
+
+    public function delProducto($isbn)
+    {
+        if (isset($carrito) && isset($carrito[$isbn])) {
+            if ($carrito[$isbn] >= 2) {
+                $carrito[$isbn]--;
+            } else {
+                unset($carrito[$isbn]);
+            }
+            return $carrito;
+        }
+    }
+>>>>>>> Stashed changes
 }
